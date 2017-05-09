a4ac35d017671b7fb9787f7946d35d3be5f66b3c
Kevin Casareo <casareo.kevin@gmail.com>
2017-05-09 21:36:47 +1000

    Rebase complete. Fixed enum minor syn error

--- app/src/main/java/com/kcasareo/beaconService/Beacons/Bluetooth/BluetoothFactory.java
+++ app/src/main/java/com/kcasareo/beaconService/Beacons/Bluetooth/BluetoothFactory.java
@@ -1,6 +1,7 @@
 package com.kcasareo.beaconService.Beacons.Bluetooth;
 
 import com.kcasareo.beaconService.Beacons.BeaconCreateDescription;
+import com.kcasareo.beaconService.Beacons.Bluetooth.BLUETOOTH_TYPE;
 
 /**
  * Created by Kevin on 5/05/2017.
@@ -14,9 +15,9 @@ public class BluetoothFactory {
     public static Bluetooth create(BeaconCreateDescription description) {
         //Bluetooth bluetooth;
         switch(description.bluetooth_type()) {
-            case BLUETOOTH_TYPE.ESTIMOTE:
+            case ESTIMOTE:
                 return new Estimote(description.id());
-            case BLUETOOTH_TYPE.SENSOR_TAG:
+            case SENSOR_TAG:
                 return new SensorTag(description.id());
             default:
                 // Return a non-reliable bluetooth sensor connection if the connection is unrecognised;

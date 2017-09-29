9a9c161dfb8ffa4ec94b59a15587d643399cf0f7
Kevin Casareo <casareo.kevin@gmail.com>
2017-09-29 11:14:33 +1000

    Creating fragments for different parts of View

--- app/src/main/java/com/kcasareo/application/GraphFragment.java
+++ app/src/main/java/com/kcasareo/application/GraphFragment.java
@@ -2,7 +2,6 @@ package com.kcasareo.application;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +12,12 @@ import com.kcasareo.ranging.R;
  * Created by Kevin on 29/09/2017.
  */
 
-public class HistoryFragment extends Fragment {
+public class GraphFragment extends Fragment {
+    private Graph graph;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.history, container, false);
+        return inflater.inflate(R.layout.graph, container, false);
     }
 }

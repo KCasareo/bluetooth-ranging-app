0c909730f38dab26e10f1c312881bd360c22f1f7
Kevin Casareo <casareo.kevin@gmail.com>
2018-01-05 23:39:57 +1100

    Added more skeleton code for modularity

---
+++ app/src/main/java/com/BeaconManager/beaconService/beacons/ScanCallbackFactory.java
@@ -0,0 +1,8 @@
+package com.BeaconManager.beaconService.beacons;
+
+/**
+ * Created by Kevin on 5/01/2018.
+ */
+
+public class ScanCallbackFactory {
+}
---
+++ app/src/main/java/com/BeaconManager/beaconService/beacons/bluetooth/BluetoothBeaconServiceBinder.java
@@ -0,0 +1,10 @@
+package com.BeaconManager.beaconService.beacons.bluetooth;
+
+import com.BeaconManager.beaconService.IBeaconService;
+
+/**
+ * Created by Kevin on 5/01/2018.
+ */
+
+public class BluetoothBeaconServiceBinder extends IBeaconService.Stub {
+}

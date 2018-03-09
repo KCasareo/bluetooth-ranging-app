9062d134b30632bb241ea314ede3c277bf2e13a9
Kevin Casareo <casareo.kevin@gmail.com>
2018-03-09 14:00:50 +1100

    Merging

---
+++ app/src/main/java/com/BeaconManager/beaconService/beacons/bluetooth/LeScanCallback.java
@@ -0,0 +1,10 @@
+package com.BeaconManager.beaconService.beacons.bluetooth;
+
+import android.bluetooth.le.ScanCallback;
+
+/**
+ * Created by Kevin on 21/01/2018.
+ */
+
+public class LeScanCallback extends ScanCallback {
+}
